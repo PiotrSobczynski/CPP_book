a349b8d0de441dd326d32bb0dba1d2453e9865f9
PiotrSobczynski <sobczynski.piotrr@gmail.com>
2015-11-09 23:37:56 +0100

    Third and Four exercise - third unit

--- Unit3_exercises/Unit3_exercises/Unit3_exercises.cpp
+++ Unit3_exercises/Unit3_exercises/Unit3_exercises.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 int main()
 {
+	/*
 	//First
 	float centimeters, feet, cal;
 	cout << "Enter height in centimeters\n";
@@ -17,10 +18,80 @@ int main()
 	feet = cal / feet_to_cal;
 	cout << centimeters << "cm = " << cal << "cal, " << feet << "feets\n";
 	cin.get();
+	*/
+	/*
+	//Second
+	const float feet_to_inches = 12.0f,
+		inches_to_meters = 0.0254f,
+		pounds_to_kg = 2.2f;
+	float inches, feet2, total_inches, height_meters, weight_pounds,
+		weight_kg, bmi;
+	cout << "Enter your weight in pounds: ";
+	cin >> weight_pounds;
+	cout << "Enter your height, enter inches: ";
+	cin >> inches;
+	cout << "Enter feet: ";
+	cin >> feet2;
+	total_inches = inches + feet2 * feet_to_inches;
+	height_meters = total_inches * inches_to_meters;
+	weight_kg = weight_pounds / pounds_to_kg;
+	bmi = weight_kg / (height_meters*height_meters);
+	cout << "Your BMI  =  " << bmi << endl;
+	cin.get();
+	cin.get();
+	*/
+	/*
+	//Third
+	const float seconds_to_minute = 60.0;
+	const float minutes_to_degree = 60.0;
+	float degrees, minutes, seconds;
+	float overall;
+	cout << "Enter degrees: ";
+	cin >> degrees;
+	cout << "\nEnter minutes: ";
+	cin >> minutes;
+	cout << "\nEnter seconds: ";
+	cin >> seconds;
+
+	overall = degrees + (minutes / minutes_to_degree) + (seconds / seconds_to_minute / minutes_to_degree);
+
+	cout << overall;
+	cin.get();
+	cin.get();
+	*/
+	//Fourth
+	const int seconds_in_minute = 60;
+	const int seconds_in_hour = 3600;
+	const int seconds_in_day = 3600 * 24;
+
+	long long seconds_1;
+	int days, hours, minutes, sec;
+	cout << "Enter number of seconds:";
+	cin >> seconds_1;
+	days = seconds_1 / seconds_in_day;
+	seconds_1 = seconds_1 % seconds_in_day;
+	hours = seconds_1 / seconds_in_hour;
+	seconds_1 = seconds_1 % seconds_in_hour;
+	minutes = seconds_1 / seconds_in_minute;
+	seconds_1 = seconds_1 % seconds_in_minute;
+	sec = seconds_1;
+
+	cout << days << " dni\n" << hours << " godzin\n" << minutes << " minut\n"
+		<< sec << " sekund\n";
+	cin.get();
 	cin.get();
 
+	//Fifth
 
-	//Second
+
+
+
+
+
+
+
+
+
 
 	return 0;
 }
